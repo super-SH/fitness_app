275a490f1409c79a7d3495bde34661c453cde521
super-SH <dev.swam.htet@gmail.com>
2023-12-26 01:01:28 +0630

    feat: add SearchExercises

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,7 +1,70 @@
-import React from 'react';
+import {
+  Button,
+  InputAdornment,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
+import React, { useState } from 'react';
 
 function SearchExercises() {
-  return <div>SearchExercises</div>;
+  const [searchTerm, setSearchTerm] = useState('');
+
+  function handleSearch() {
+    console.log('clicked');
+    if (!searchTerm) return;
+    console.log(searchTerm);
+  }
+
+  return (
+    <Stack
+      alignItems='center'
+      justifyContent='center'
+      mt='32px'
+      gap='32px'
+      p='20px'
+    >
+      <Typography
+        sx={{ fontSize: { xs: '28px', lg: '42px' } }}
+        fontWeight={700}
+        textAlign='center'
+      >
+        Awesome Exercises You <br /> Should Know
+      </Typography>
+
+      {/* Color error are temporary , change later */}
+      <TextField
+        hiddenLabel
+        variant='filled'
+        placeholder='Search Exercises'
+        type='text'
+        color='error'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value.toLowerCase())}
+        sx={{
+          input: {
+            fontWeight: '700',
+            border: 'none',
+          },
+          width: { xs: '324px', lg: '966px' },
+        }}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position='end'>
+              <Button
+                variant='contained'
+                color='error'
+                sx={{ height: '48px' }}
+                onClick={handleSearch}
+              >
+                Search
+              </Button>
+            </InputAdornment>
+          ),
+        }}
+      />
+    </Stack>
+  );
 }
 
 export default SearchExercises;
